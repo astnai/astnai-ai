c86c466d29b11190b59a0d62fdc83c7141b7f171
astnai <aa@agustinarias.com>
2024-07-10 16:56:35 -0300

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,94 +1,12 @@
 import "./globals.css";
 import { GeistSans } from "geist/font/sans";
-import { Metadata, Viewport } from "next";
 import Head from "next/head";
 
 const geist = GeistSans;
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://ai.agustinarias.com"),
-  title: {
-    default: "astnai",
-    template: "%s | astnai",
-  },
-  description: "chat with astnai, your personal ai assistant by agustín arias.",
-  openGraph: {
-    title: "astnai",
-    description:
-      "chat with astnai, your personal ai assistant by agustín arias.",
-    url: "https://ai.agustinarias.com",
-    siteName: "astnai",
-    locale: "en_us",
-    type: "website",
-    images: [
-      {
-        url: "https://ai.agustinarias.com/images/og-image.jpg",
-        width: 1200,
-        height: 630,
-        alt: "astnai",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    creator: "@astnai",
-    title: "astnai",
-    description:
-      "chat with astnai, your personal ai assistant by agustín arias.",
-    images: "https://ai.agustinarias.com/images/twitter-image.jpg",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  alternates: {
-    canonical: "https://ai.agustinarias.com",
-  },
-};
-
-export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-  viewportFit: "cover",
-  height: "device-height",
-};
-
-const jsonLdData = {
-  "@context": "https://schema.org",
-  "@type": "Person",
-  name: "agustín arias",
-  url: "https://ai.agustinarias.com",
-  sameAs: [
-    "https://www.linkedin.com/in/astnai",
-    "https://twitter.com/astnai",
-    "https://github.com/astnai",
-    "https://instagram.com/agustnarias",
-  ],
-  jobTitle: "software engineer",
-  worksFor: {
-    "@type": "Organization",
-    name: "agustín arias",
-  },
-  description:
-    "agustín arias is a software engineer based in buenos aires, originally from patagonia, argentina. he is passionate about web development and ai interaction.",
-  image: "https://ai.agustinarias.com/images/profile.jpg",
-  address: {
-    "@type": "PostalAddress",
-    addressLocality: "buenos aires",
-    addressRegion: "ba",
-    addressCountry: "argentina",
-  },
-  alumniOf: "self-taught",
-  birthPlace: "patagonia, argentina",
+export const metadata = {
+  title: "astnai | assistant project",
+  description: "your personalized assistant for all things agustín arias",
 };
 
 export default function RootLayout({
@@ -97,58 +15,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="author" content="agustín arias" />
-        <meta
-          name="keywords"
-          content="software engineer, buenos aires, ai, web development, patagonia, mate, argentina, technology, san francisco, y combinator, guillermo rauch, sam altman, messi, duki"
-        />
-        <meta
-          name="description"
-          content="chat with astnai, your personal ai assistant by agustín arias."
-        />
-        <meta property="og:title" content="astnai" />
-        <meta
-          property="og:description"
-          content="chat with astnai, your personal ai assistant by agustín arias."
-        />
-        <meta
-          property="og:image"
-          content="https://ai.agustinarias.com/images/og-image.jpg"
-        />
-        <meta property="og:url" content="https://ai.agustinarias.com" />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content="@astnai" />
-        <meta name="twitter:title" content="astnai" />
-        <meta
-          name="twitter:description"
-          content="chat with astnai, your personal ai assistant by agustín arias."
-        />
-        <meta
-          name="twitter:image"
-          content="https://ai.agustinarias.com/images/twitter-image.jpg"
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="canonical" href="https://ai.agustinarias.com" />
-        <link
-          rel="alternate"
-          href="https://ai.agustinarias.com"
-          hrefLang="en"
-        />
-        <link
-          rel="alternate"
-          href="https://ai.agustinarias.com/es"
-          hrefLang="es"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <script type="application/ld+json">{JSON.stringify(jsonLdData)}</script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <body className={`${GeistSans.className} antialiased`}>{children}</body>
+      <body className={geist.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,20 @@
 
 import React, { useEffect, useRef, useState } from "react";
 import { Message, useAssistant } from "ai/react";
-import { FaArrowCircleUp } from "react-icons/fa";
+import { FaArrowUp } from "react-icons/fa";
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import logoMeBlack from "./logoMeBlack.png";
 import { GeistSans } from "geist/font/sans";
 
 const geistSans = GeistSans;
+
+const predefinedQuestions = [
+  "tell me your twitter",
+  "who are your idols?",
+  "where do you live?",
+  "what is your job",
+];
 
 export default function Chat() {
   const { status, messages, input, submitMessage, handleInputChange } =
@@ -20,6 +27,7 @@ export default function Chat() {
   const [animatedMessages, setAnimatedMessages] = useState<Set<string>>(
     new Set()
   );
+  const [showCards, setShowCards] = useState(true);
 
   useEffect(() => {
     setMounted(true);
@@ -43,7 +51,8 @@ export default function Chat() {
       chatContainerRef.current.scrollTop =
         chatContainerRef.current.scrollHeight;
     }
-    setShowLogo(messages.length === 0 && status === "awaiting_message");
+    setShowLogo(messages.length === 0);
+    setShowCards(messages.length === 0);
 
     const newMessages = messages.filter((m) => !animatedMessages.has(m.id));
     if (newMessages.length > 0) {
@@ -57,13 +66,21 @@ export default function Chat() {
     }
   }, [status, messages, animatedMessages]);
 
+  const handleCardClick = (question: string) => {
+    handleInputChange({
+      target: { value: question },
+    } as React.ChangeEvent<HTMLInputElement>);
+    inputRef.current?.focus();
+    setShowCards(false);
+  };
+
   if (!mounted) return null;
   return (
     <div
       className={`${geistSans.className} w-full h-screen flex flex-col items-center justify-center bg-white`}
     >
-      <div className="w-full h-full max-w-2xl flex flex-col bg-white overflow-hidden p-4 mb-20 mx-auto">
-        <div className="flex-1 overflow-hidden flex flex-col relative chat-container">
+      <div className="w-full h-full max-w-2xl flex flex-col bg-white overflow-hidden p-4 mb-20 mx-auto relative">
+        <div className="flex-1 overflow-hidden flex flex-col">
           <div
             className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-opacity duration-500 z-10 ${
               showLogo ? "opacity-100" : "opacity-0"
@@ -90,85 +107,81 @@ export default function Chat() {
                 }`}
               >
                 <div
-                  className={`${
+                  className={`max-w-[80%] break-words p-3 rounded-full ${
                     m.role === "user"
-                      ? "bg-[#1c1c1c] text-white rounded-full p-2"
-                      : "text-[#1c1c1c] rounded-full p-2"
+                      ? "bg-neutral-100 text-black"
+                      : "bg-transparent text-black"
                   } ${
                     m.role === "assistant" && !animatedMessages.has(m.id)
                       ? "opacity-0"
                       : ""
-                  } max-w-[80%] break-words`}
+                  }`}
                 >
-                  {m.role === "assistant" ? (
-                    <div className="assistant-message">
-                      <div className="flex-shrink-0 mr-2">
-                        <Image
-                          src={logoMeBlack}
-                          alt="Astnai"
-                          width={24}
-                          height={24}
+                  <ReactMarkdown
+                    className="inline-block text-sm whitespace-pre-wrap"
+                    components={{
+                      a: ({ node, ...props }) => (
+                        <a
+                          {...props}
+                          className="text-black underline hover:text-black transition-colors duration-300"
+                          target="_blank"
+                          rel="noopener noreferrer"
                         />
-                      </div>
-                      <ReactMarkdown
-                        className="inline-block text-sm whitespace-pre-wrap"
-                        components={{
-                          a: ({ node, ...props }) => (
-                            <a
-                              {...props}
-                              className="text-blue-500 hover:underline"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            />
-                          ),
-                        }}
-                      >
-                        {m.content.replace(/【.*?】/g, "")}
-                      </ReactMarkdown>
-                    </div>
-                  ) : (
-                    <ReactMarkdown
-                      className="inline-block text-sm whitespace-pre-wrap"
-                      components={{
-                        a: ({ node, ...props }) => (
-                          <a
-                            {...props}
-                            className="text-blue-500 hover:underline"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          />
-                        ),
-                      }}
-                    >
-                      {m.content.replace(/【.*?】/g, "")}
-                    </ReactMarkdown>
-                  )}
+                      ),
+                    }}
+                  >
+                    {m.content.replace(/【.*?】/g, "")}
+                  </ReactMarkdown>
                 </div>
               </div>
             ))}
           </div>
         </div>
-
-        <div className="input-container max-w-2xl mx-auto w-full">
-          <form onSubmit={submitMessage} className="relative flex items-center">
+        <div
+          className={`grid grid-cols-2 gap-4 mb-36 transition-opacity duration-300 ${
+            showCards ? "opacity-100" : "opacity-0 pointer-events-none"
+          }`}
+        >
+          {predefinedQuestions.map((question, index) => (
+            <div
+              key={index}
+              className="bg-neutral-100 rounded-full p-4 cursor-pointer transition-all duration-300 hover:bg-neutral-200 text-center text-sm"
+              onClick={() => handleCardClick(question)}
+            >
+              {question}
+            </div>
+          ))}
+        </div>
+        <div className="fixed bottom-0 left-0 right-0 bg-white p-5">
+          <form
+            onSubmit={submitMessage}
+            className="relative flex items-center w-full max-w-2xl mx-auto"
+          >
             <input
               ref={inputRef}
               disabled={status !== "awaiting_message"}
-              className="flex-grow p-2 pr-12 bg-[#1c1c1c] text-white rounded-full focus:outline-none placeholder-white"
+              className="flex-grow p-3 bg-neutral-100 text-black rounded-full placeholder-neutral-400 transition-all duration-300 focus:outline-none focus:ring-0 focus:border-transparent"
               value={input}
-              placeholder=""
+              placeholder="Type your message..."
               onChange={handleInputChange}
             />
             <button
               type="submit"
               disabled={status !== "awaiting_message"}
-              className="absolute right-2 p-2 text-white hover:text-neutral-600 focus:outline-none transition-colors duration-200"
+              className="absolute right-2 p-2 bg-black text-white rounded-full hover:bg-neutral-800"
             >
-              <FaArrowCircleUp className="w-6 h-6" />
+              <FaArrowUp className="w-4 h-4" />
             </button>
           </form>
-          <p className="text-center text-xs font-mono mt-2 mb-1 text-black">
-            talk to astnai
+          <p className="text-center text-xs mt-4 text-neutral-400">
+            astnai provides information about{" "}
+            <a
+              href="https://agustinarias.com"
+              className="underline hover:text-black transition-colors duration-300"
+            >
+              agustin arias
+            </a>
+            .
           </p>
         </div>
       </div>
